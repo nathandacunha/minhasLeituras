606169a8b48ac059eefb308e8724ecdda6575202
nathandacunha <nathandacunha233@gmail.com>
2026-07-11 05:01:12 -0300

    feat(import): Verificação de notas e adiciona os livros lidos do ano até agora

--- scripts/import.r
+++ scripts/import.r
@@ -4,27 +4,36 @@ install.packages("readr")
 install.packages("dplyr")
 
 # Importação dos pacotes
-
 library(readr)
 library(here)
 library(dplyr)
 
-# here("data", "leituras.csv") monta o caminho a partir da pasta raiz do projeto
 caminho_arquivo <- here::here("data", "leituras.csv")
 
 leituras <- read_delim(
-    caminho_arquivo,
-    delim = ";",
-    locale = locale(decimal_mark = ",", grouping_mark = "."),
-    col_types = cols(
-        titulo = col_character(),
-        autor = col_character(),
-        genero = col_character(),
-        nota = col_double(),
-        paginas = col_double()
-    )
+  caminho_arquivo,
+  delim = ";",
+  trim_ws = TRUE,
+  col_types = cols(
+    titulo  = col_character(),
+    autor   = col_character(),
+    genero  = col_character(),
+    nota    = col_double(),
+    paginasOuCapitulos = col_double(),
+    tipo = col_character()
+  )
 )
 
-# checagem dos dados 
+# checagem dos dados
 glimpse(leituras)
 print(head(leituras))
+
+# validações simples
+
+# verificação se a nota está fora da escala 0 a 10
+notas_invalidas <- leituras %>%
+  filter(nota < 0 | nota > 10)
+
+if (nrow(notas_invalidas) > 0) {
+  warning("Existem notas fora de escala (0 a 10)")
+}
